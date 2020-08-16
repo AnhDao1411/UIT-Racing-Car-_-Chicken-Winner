8aced9ba1d6f4235d199325ff15aaed38492597c
anhdao14112000 <dao.nguyen@f4.intel.edu.vn>
2020-08-16 14:13:11 +0700

    code_test_simulation

---
+++ Code test Simulation/Raw code/raw_code.py
@@ -0,0 +1,88 @@
+import base64
+import numpy as np
+import socketio
+import eventlet
+import eventlet.wsgi
+import cv2
+from PIL import Image
+from flask import Flask
+from io import BytesIO
+#------------- Add library ------------#
+
+#--------------------------------------#
+
+# initialize our server
+sio = socketio.Server()
+# our flask (web) app
+app = Flask(__name__)
+# registering event handler for the server
+
+
+@sio.on('telemetry')
+def telemetry(sid, data):
+    if data:
+
+        steering_angle = 0  # Góc lái hiện tại của xe
+        speed = 0  # Vận tốc hiện tại của xe
+        image = 0  # Ảnh gốc
+
+        steering_angle = float(data["steering_angle"])
+        speed = float(data["speed"])
+        # Original Image
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+        image = np.asarray(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        """
+        - Chương trình đưa cho bạn 3 giá trị đầu vào:
+            * steering_angle: góc lái hiện tại của xe
+            * speed: Tốc độ hiện tại của xe
+            * image: hình ảnh trả về từ xe
+        
+        - Bạn phải dựa vào 3 giá trị đầu vào này để tính toán và gửi lại góc lái và tốc độ xe cho phần mềm mô phỏng:
+            * Lệnh điều khiển: send_control(sendBack_angle, sendBack_Speed)
+            Trong đó:
+                + sendBack_angle (góc điều khiển): [-25, 25]  NOTE: ( âm là góc trái, dương là góc phải)
+                + sendBack_Speed (tốc độ điều khiển): [-150, 150] NOTE: (âm là lùi, dương là tiến)
+        """
+
+        sendBack_angle = 0
+        sendBack_Speed = 0
+        try:
+            #------------------------------------------  Work space  ----------------------------------------------#
+
+            cv2.waitKey(1)
+            #------------------------------------------------------------------------------------------------------#
+            print('{} : {}'.format(sendBack_angle, sendBack_Speed))
+            send_control(sendBack_angle, sendBack_Speed)
+        except Exception as e:
+            print(e)
+    else:
+        sio.emit('manual', data={}, skip_sid=True)
+
+
+@sio.on('connect')
+def connect(sid, environ):
+    print("connect ", sid)
+    send_control(0, 0)
+
+
+def send_control(steering_angle, throttle):
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__(),
+        },
+        skip_sid=True)
+
+
+if __name__ == '__main__':
+
+    #-----------------------------------  Setup  ------------------------------------------#
+
+    #--------------------------------------------------------------------------------------#
+    # wrap Flask application with engineio's middleware
+    app = socketio.Middleware(sio, app)
+    # deploy as an eventlet WSGI server
+    eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
